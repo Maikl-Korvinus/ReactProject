4799aecc200b1ec72d47f281fafbf61046f1413c
Maikl-Korvinus <diskord.karas@bk.ru>
2021-08-14 13:42:51 +0300

    first commit in reactProject

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = (props) => {
+
+  return (
+    <div className={s.item}>
+      <img src='https://www.meme-arsenal.com/memes/8ee21821bfa041ce285b476e57835a56.jpg' />
+      {props.message}
+      <div>
+        <span>like</span>
+      </div>
+    </div>
+  )
+}
+
+export default Post;

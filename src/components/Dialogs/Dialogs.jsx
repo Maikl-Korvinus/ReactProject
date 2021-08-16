0b335f774043844f9be42d17848dce0df9965e27
Maikl-Korvinus <diskord.karas@bk.ru>
2021-08-16 16:32:03 +0300

    mini-verstka dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
    return (
-      <div  >
-         Dialogs
+      <div className={s.dialogs}>
+         <div className={s.dialogsItems}>
+            <div className={s.dialog + " " + s.active}>
+               Maikle
+            </div>
+            <div className={s.dialog}>
+               Trofim
+            </div>
+            <div className={s.dialog}>
+               Polya
+            </div>
+            <div className={s.dialog}>
+               Ilui
+            </div>
+            <div className={s.dialog}>
+               Sanya
+            </div>
+         </div>
+         <div className={s.messages}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>Whats up</div>
+            <div className={s.message}>Aloha</div>
+         </div>
       </div>
    )
 }

adfe5fc0e1d9c08c9dbefdca0ee92a2ca74dddca
Maikl-Korvinus <diskord.karas@bk.ru>
2021-08-22 18:11:01 +0300

    Flux circulation

--- src/redux/state.js
+++ src/redux/state.js
@@ -9,6 +9,7 @@ let state = {
          { id: 2, message: "Bla bla", likesCount: 13, },
          { id: 2, message: "DdDa", likesCount: 15, },
       ],
+      newPostText: 'hard work for better future',
    },
    dialogsPage: {
       message: [
@@ -28,13 +29,20 @@ let state = {
    }
 }
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
    let newPost = {
       id: 5,
-      message: postMessage,
+      message: state.profilePage.newPostText,
       likesCount: 0,
    }
    state.profilePage.posts.push(newPost);
+   state.profilePage.newPostText = '';
+   rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+
+   state.profilePage.newPostText = newText;
    rerenderEntireTree(state);
 }
 

785c5b22914f130f233cc8eeb7f74a0befde8a3a
Maikl-Korvinus <diskord.karas@bk.ru>
2021-08-23 14:00:41 +0300

    callback subscribe observer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+
+}
 
 
 let state = {
@@ -29,7 +31,7 @@ let state = {
    }
 }
 
-export let addPost = () => {
+export const addPost = () => {
    let newPost = {
       id: 5,
       message: state.profilePage.newPostText,
@@ -40,10 +42,13 @@ export let addPost = () => {
    rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
 
    state.profilePage.newPostText = newText;
    rerenderEntireTree(state);
+}
+export const subscribe = (observer) => {
+   rerenderEntireTree = observer;
 }
 
 export default state;
